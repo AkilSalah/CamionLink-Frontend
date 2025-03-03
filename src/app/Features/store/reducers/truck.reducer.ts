0fa8af41f7ee801622f170ae717a781555bb0364
salah <akilsalaheddine203@gmail.com>
2025-03-03 12:25:18 +0100

    add truck reducers

--- src/app/Features/store/reducers/truck.reducer.ts
+++ src/app/Features/store/reducers/truck.reducer.ts
@@ -1,17 +1,96 @@
 import { createReducer, on } from '@ngrx/store';
-import { TruckActions } from './truck.actions';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+import * as TruckActions from '../actions/truck.actions';
+import { Camion } from '../../../Core/models/camion.model';
 
-export const truckFeatureKey = 'truck';
-
-export interface State {
-
+export interface TruckState extends EntityState<Camion> {
+  loading: boolean;
+  error: any;
 }
 
-export const initialState: State = {
+export const adapter: EntityAdapter<Camion> = createEntityAdapter<Camion>({
+  selectId: (truck: Camion) => truck.id
+});
 
-};
+export const initialState: TruckState = adapter.getInitialState({
+  loading: false,
+  error: null
+});
 
-export const reducer = createReducer(
+export const truckReducer = createReducer(
   initialState,
+  on(TruckActions.loadTruck, (state) => ({
+    ...state,
+    loading: true,
+    error: null
+  })),
+  on(TruckActions.loadTrucksSuccess, (state, { trucks }) => 
+    adapter.setAll(trucks, {
+      ...state,
+      loading: false
+    })
+  ),
+  on(TruckActions.loadTrucksFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error
+  })),
+
+  on(TruckActions.addTruck, (state) => ({
+    ...state,
+    loading: true
+  })),
+  on(TruckActions.addTruckSuccess, (state, { truck }) => 
+    adapter.addOne(truck, {
+      ...state,
+      loading: false
+    })
+  ),
+  on(TruckActions.addTruckFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error
+  })),
+
+  on(TruckActions.updateTruck, (state) => ({
+    ...state,
+    loading: true
+  })),
+  on(TruckActions.updateTruckSuccess, (state, { truck }) => 
+    adapter.updateOne(
+      { id: truck.id, changes: truck },
+      {
+        ...state,
+        loading: false
+      }
+    )
+  ),
+  on(TruckActions.updateTruckFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error
+  })),
+
+  on(TruckActions.deleteTruck, (state) => ({
+    ...state,
+    loading: true
+  })),
+  on(TruckActions.deleteTruckSuccess, (state, { id }) => 
+    adapter.removeOne(id, {
+      ...state,
+      loading: false
+    })
+  ),
+  on(TruckActions.deleteTruckFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error
+  }))
 );
 
+export const {
+  selectIds,
+  selectEntities,
+  selectAll,
+  selectTotal
+} = adapter.getSelectors();

18389ab778df231c2d83a1e07880aa23723a1424
salah <akilsalaheddine203@gmail.com>
2025-03-03 12:29:18 +0100

    add truck selectors

--- src/app/Features/store/selectors/truck.selectors.ts
+++ src/app/Features/store/selectors/truck.selectors.ts
@@ -1,2 +1,29 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import * as fromTruck from '../reducers/truck.reducer';
 
+export const selectTruckState = createFeatureSelector<fromTruck.TruckState>('trucks');
+
+export const selectAllTrucks = createSelector(
+  selectTruckState,
+  fromTruck.selectAll
+);
+
+export const selectTruckEntities = createSelector(
+  selectTruckState,
+  fromTruck.selectEntities
+);
+
+export const selectTruckLoading = createSelector(
+  selectTruckState,
+  state => state.loading
+);
+
+export const selectTruckError = createSelector(
+  selectTruckState,
+  state => state.error
+);
+
+export const selectTruckById = (id: number) => createSelector(
+  selectTruckEntities,
+  entities => entities[id]
+);

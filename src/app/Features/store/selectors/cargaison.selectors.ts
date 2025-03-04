aebd917668a29ce684d09f24222b4d27c9c89a83
salah <akilsalaheddine203@gmail.com>
2025-03-04 12:03:54 +0100

    add cargaison selectors

--- src/app/Features/store/selectors/cargaison.selectors.ts
+++ src/app/Features/store/selectors/cargaison.selectors.ts
@@ -1,2 +1,29 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import * as fromCargaison from '../reducers/cargaison.reducer';
 
+export const selectCargaisonState = createFeatureSelector<fromCargaison.CargaisonState>('cargaisons');
+
+export const selectAllCargaison = createSelector(
+  selectCargaisonState,
+  fromCargaison.selectAll
+);
+
+export const selectCargaisonEntities = createSelector(
+  selectCargaisonState,
+  fromCargaison.selectEntities
+);
+
+export const selectCargaisonLoading = createSelector(
+  selectCargaisonState,
+  state => state.loading
+);
+
+export const selectCargaisonError = createSelector(
+  selectCargaisonState,
+  state => state.error
+);
+
+export const selectCargaisonById = (id: number) => createSelector(
+  selectCargaisonEntities,
+  entities => entities[id]
+);

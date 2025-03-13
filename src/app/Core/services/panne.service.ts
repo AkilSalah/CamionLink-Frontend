683c3e4ddf45e39a4fc1ec1dec8c5d79d56df1ae
salah <akilsalaheddine203@gmail.com>
2025-03-13 23:31:35 +0100

    add panne service

--- src/app/Core/services/panne.service.ts
+++ src/app/Core/services/panne.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Panne } from '../models/panne.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PanneService {
 
-  constructor() { }
+  private adminBaseUrl = "localhost:8086/api/admin/pannes";
+  private conducteurBaseUrl = "localhost:8086/api/conducteur/pannes"
+
+  constructor(private http : HttpClient) { }
+  
+  createPanne(panne: Panne): Observable<Panne> {
+    return this.http.post<Panne>(`${this.conducteurBaseUrl}`, panne);
+  }
+
+  getPannesByTrajet(trajetId: number): Observable<Panne[]> {
+    return this.http.get<Panne[]>(`${this.conducteurBaseUrl}/trajet/${trajetId}`);
+  }
+  deletePanne(panneId: number): Observable<void> {
+    return this.http.delete<void>(`${this.conducteurBaseUrl}/${panneId}`);
+  }
+
+  getPanneById(panneId: number): Observable<Panne> {
+    return this.http.get<Panne>(`${this.adminBaseUrl}/${panneId}`);
+  }
+  
+ getAllPannes(): Observable<Panne[]> {
+    return this.http.get<Panne[]>(this.adminBaseUrl);
+  }
+
 }
